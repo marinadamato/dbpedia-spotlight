d1d43a48e592526fab107236ad21a4f47da8ade0
giuseppefutia <giuseppe.futia@gmail.com>
2015-12-27 15:15:47 +0100

    Correct bugs in services script

--- bin/index.tmf.domain.services.sh
+++ bin/index.tmf.domain.services.sh
@@ -2,5 +2,7 @@
 # Each service is wrapped by the Fusepool Transformer in order to standardize each service
 
 # Pull up domain services
+export INDEX_CONFIG_FILE=../conf/indexing.tmf.domain.en.properties
+cd ../domain-services
 mvn compile
-mvn exec:java -e -Dexec.mainClass="eu.fusepool.p3.transformer.LDRService" -Dexec.args="$INDEX_CONFIG_FILE"
+mvn exec:java -e -Dexec.mainClass="org.dbpedia.spotlight.LDRService" -Dexec.args="$INDEX_CONFIG_FILE"
