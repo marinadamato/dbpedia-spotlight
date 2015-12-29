ccc99dc96ce4ac7a9b2a33e68f4e1903683906cb
giuseppefutia <giuseppe.futia@gmail.com>
2015-12-29 19:19:53 +0100

    Clean bad import

--- index/src/main/java/org/dbpedia/spotlight/lucene/index/external/domain/TMFDomainEngine.java
+++ index/src/main/java/org/dbpedia/spotlight/lucene/index/external/domain/TMFDomainEngine.java
@@ -13,7 +13,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.*;
 
 /**
